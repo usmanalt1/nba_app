fe57a058cba01637bb6138b35f1eaccf06647178
Usman <usman@Usmans-MacBook-Pro.local>
2026-07-01 21:44:09 +0100

    interfaces

--- frontend/src/components/DataTable/NBADataTable.tsx
+++ frontend/src/components/DataTable/NBADataTable.tsx
@@ -2,15 +2,7 @@ import { DataTable, type DataTableSortStatus } from 'mantine-datatable';
 import { useMemo, useState } from 'react';
 import sortBy from 'lodash/sortBy';
 import 'mantine-datatable/styles.layer.css';
-
-
-interface PlayerStats {
-    season: string;
-    points: number;
-    rebounds: number;
-    plusMinus: number;
-    assists: number;
-}
+import type { PlayerStats } from '../../types/player';
 
 export default function NBADataTable({ nbaData = [] }: { nbaData: PlayerStats[] }) {
     const [sortStatus, setSortStatus] = useState<DataTableSortStatus<PlayerStats>>({
@@ -31,11 +23,11 @@ export default function NBADataTable({ nbaData = [] }: { nbaData: PlayerStats[] 
       records={records}
       emptyState={null}
       columns={[
-        { accessor: 'season', width: '20%', sortable: true },
+        { accessor: 'season', width: '10%', sortable: true },
         { accessor: 'points', width: '10%', sortable: true },
         { accessor: 'rebounds', width: '10%', sortable: true },
-        { accessor: 'plusMinus', width: '15%', sortable: true, textAlign: 'right' },
-        { accessor: 'assists', width: '15%', sortable: true, textAlign: 'right' },
+        { accessor: 'plusMinus', width: '10%', sortable: true, textAlign: 'right' },
+        { accessor: 'assists', width: '10%', sortable: true, textAlign: 'right' },
       ]}
       sortStatus={sortStatus}
       onSortStatusChange={setSortStatus}
