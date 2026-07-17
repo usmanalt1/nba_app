bec3f29d52439e03dad1f8827082461b9e30bf5e
Usman <usman@Usmans-MacBook-Pro.local>
2026-07-17 19:55:46 +0100

    v1

---
+++ frontend/src/components/Home/HomeTabs.tsx
@@ -0,0 +1,34 @@
+import { Tabs } from '@mantine/core';
+import { useState } from 'react';
+import HomePlayerStats from "./HomePlayerStats";
+
+
+export default function HomeTabs() {
+  const [activeTab, setActiveTab] = useState('player-stats');
+
+  const typeTypes = [
+    { value: 'player-stats', label: 'Player Stats' },
+    { value: 'team-stats', label: 'Team Stats' },
+    { value: 'season-stats', label: 'Season Stats' },
+    { value: 'advanced-stats', label: 'Advanced Stats' }
+  ];
+
+  const onClickTab = (value: string) => {
+    setActiveTab(value);
+  }
+
+  return (
+    <><Tabs variant="pills" style={{ width: "100%", marginBottom: '30px' }} defaultValue="player-stats">
+      <Tabs.List grow={true} style={{ width: "100%", display: 'flex', justifyContent: 'space-between' }}>
+        {typeTypes.map(type =>
+          <Tabs.Tab
+            onClick={() => onClickTab(type.value)} value={type.value} style={{ fontWeight: 700, fontSize: '16px' }}>
+            {type.label}
+          </Tabs.Tab>
+        )}
+      </Tabs.List>
+    </Tabs>
+      {activeTab === 'player-stats' && <HomePlayerStats />}
+    </>
+  );
+}
